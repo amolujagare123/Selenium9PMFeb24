ecd3896bf9078f8d12d292489c3f41cf21dde9d5
amolu <amolujagare@gmail.com>
2024-02-29 22:05:51 +0530

    selenium

---
+++ src/xpathDemo/DataTableDemo1.java
@@ -0,0 +1,24 @@
+package xpathDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class DataTableDemo1 {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://editor.datatables.net/examples/simple/inTableControls.html");
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,350)");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.findElement(By.xpath("//tr[td[text()='Cara Stevens']]//i[contains(@class,'pencil')]")).click();
+    }
+}
